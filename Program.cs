da68b451cd8ffb9df4189908e6f2f330cfa78ea3
Anna Chesnokova <chesnokova33@list.ru>
2024-03-15 22:15:13 +0300

    add program files

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿void ShowArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("] ");
+    Console.WriteLine();
+}
+
+string[] CreateArray(int length)
+{
+    string[] myArray = new string[length];
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine("Input an element of array: ");
+        myArray[i] = Console.ReadLine();
+    }
+    return myArray;
+}
+
+string[] CreateNewArrayOfThreeLengthElements(string[] array)
+{
+    int count = 1;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i].Length <= 3) count++;
+    string[] newArray = new string[count];
+    if (count > 1)
+    {
+        int j = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length <= 3)
+            {
+                newArray[j] = array[i];
+                j++;
+            }
+        }
+    }
+    else newArray[0] = "You haven't input anything! ";
+    return newArray;
+}
